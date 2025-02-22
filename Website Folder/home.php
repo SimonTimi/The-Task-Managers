eed9f397b0a561e09ff3f6643424343df5417de5
alex33y <152503680+alex33y@users.noreply.github.com>
2025-02-22 00:19:21 +0000

    Add files via upload
    Home and Register Page, along with necessary images and css

---
+++ Website Folder/home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Gaming | Home </title>
+     <link rel="stylesheet" href="css/mvp.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<header class="homeheader" id="forbackground">
+  <div class="containertop">
+  <a href="home.html"><img src="images/orangelogo.png" alt="Scuf Logo" class="headimage" style="height: 151px; width: 151px;"></a>
+</div>
+<h1 id="homeh1">Scuf Gaming</h1>
+</header>
+        <nav class="homenav">
+            <ul>
+                <li><a href="home.html"><i class="fa fa-home"></i> Home</a></li>
+                <li><a href="products.html"><i class="fa fa-shopping-cart"></i>Products</a></li>
+                <li><a href="sitemap.html"><i class="fa fa-map"></i>Site Map </a></li>
+                <li><a href="contactus.html"><i class="fa fa-paper-plane"></i>Contact Us </a></li>
+                <li><a href="aboutus.html"><i class="fa fa-question-circle"></i>About Us </a></li>
+                <li><a href="register.php"><i class="fa fa-reg-icon"></i>Register </a></li>
+        </ul>
+        </nav>
+
+        <nav class="sidenav">
+          <ul>
+              <li class="featured"><a href="#featured">Featured</a>
+              <ul>
+                <li class="inner"><a href="https://scufgaming.com/eu/customize-your-scuf">Customizable Controllers</a></li>
+                <li class="inner"><a href="https://scufgaming.com/eu/loadout-backpack">Backpack</a></li>
+              </ul>
+            </li>
+              <li class="ps"><a href="#ps">PlayStation</a>
+                  <ul>
+                      <li class="inner"><a href="https://scufgaming.com/eu/playstation5-scuf-reflex?gad_source=1&gclid=CjwKCAiApaarBhB7EiwAYiMwqizFi8fpxhg-OsVvBod_Wan-Q2YhZjZ5sgX3sBamgSQnsp5QsLmdNRoCrDMQAvD_BwE">PlayStation Controllers</a></li>
+                      <li class="inner"><a href="https://store.playstation.com/en-us/category/5bb9a9b4-f9f4-454e-bf57-0a74bc3a7ee9/1">PlayStation Exclusives</a></li>
+                  </ul>
+              </li>
+              <li class="xbox">
+                  <a href="#xbox">Xbox</a>
+                  <ul>
+                      <li class="inner" id="instinct"><a href="https://scufgaming.com/eu/xbox-scuf-instinct">Xbox Controllers</a></li>
+                      <li class="inner"><a href="https://www.xbox.com/en-IE/xbox-game-pass">Game Pass</a></li>
+                  </ul>
+              </li>
+          </ul>
+      </nav>
+
+        <div id="carousel-container">
+          <div class="carousel">
+              <img src="images/cushion.jpg" alt="scuf cushion">
+              <img src="images/controllerblack.png" alt="black controller">
+              <img src="images/2controllers.jpg" alt="2 controller">
+          </div>
+          <div class="arrow prev" onclick="prevSlide()"><i class="fa fa-chevron-left"></i></div>
+          <div class="arrow next" onclick="nextSlide()"><i class="fa fa-chevron-right"></i></div>
+        </div>
+
+
+          <p>Founded in 2011, SCUF Gaming "SCUF®", is a global innovator and creator of high-performance gaming controllers, providing superior accessories and customized gaming controllers for console and PC that are used by top professional gamers as well as casual gamers. Built to specification, SCUF controllers offer a number of functional and design features custom built to increase hand use and improve gameplay.
+
+            SCUF controller features are covered by 105 granted patents, and another 56 pending, focusing on four key areas of a controller: the back-control functions and handles, the trigger control mechanisms, the thumbstick control area and the side-mounted configurable Sax™ button placements.</p>
+          
+
+          <script src="cascript.js"></script>
+</body>
+
+<footer>
+      <a href="https://scufgaming.com/eu/"><img src="images/bwlogo.jpg" alt="scuf Logo" style="height: 25px;">
+      <a href="https://www.facebook.com/ScufGaming/"><img src="images/facebookbw.png" alt="scuf facebook page" style="height: 25px;"></a>
+      <a href="https://twitter.com/ScufGaming?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><img src="images/xbw.png" alt="scuf x page" style="height: 25px;"></a>
+    <p>© 2010-2023 Corsair Memory, Inc.</p>
+    </a>
+</footer>
+</html>
